f1d4da16ebdf781885c1c886da5844f98d5e9ff5
Guillermo Gutiérrez <guillegr123@gmail.com>
2017-10-08 17:39:30 -0600

    Vue 2 to-do list, based on the vue-cli webpack template, from tutorial https://scotch.io/tutorials/build-a-to-do-app-with-vue-js-2, up to item edition.

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        id: 1,
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        id: 2,
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        id: 3,
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        id: 4,
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
