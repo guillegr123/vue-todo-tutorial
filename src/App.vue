b48c162d2ee516a17b6be8ff7023e91b1e2c8882
Guillermo Gutiérrez <guillegr123@gmail.com>
2017-10-08 19:34:05 -0600

    Implemented add to-do, and added sweetalert component.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="createTodo"></create-todo>
   </div>
 </template>
 
 <script>
+import sweetalert from 'sweetalert';
 import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
 
 export default {
   name: 'app',
   components: {
     TodoList,
+    CreateTodo,
   },
   data() {
     return {
@@ -36,6 +40,12 @@ export default {
         done: false,
       }],
     };
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
   },
 };
 </script>
